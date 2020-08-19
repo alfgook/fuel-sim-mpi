c32b35a4c887816835497e71477a934e392e7627
alfgook <gook@kth.se>
2020-08-19 16:02:16 +0200

    added input files to repo

---
+++ include/AnalysisMPI.hh
@@ -0,0 +1,71 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+/// @file AnalysisMPI.hh
+/// @brief Define histograms
+
+#ifndef ANALYSIS_MANAGER_H
+#define ANALYSIS_MANAGER_H
+
+#include "G4ThreeVector.hh"
+#include <tools/histo/h1d>
+#include <tools/histo/h2d>
+
+
+#define DISALLOW_COPY_AND_ASSIGN(TypeName) \
+  TypeName(const TypeName&);               \
+  void operator=(const TypeName&)
+
+class AnalysisMPI {
+public:
+  ~AnalysisMPI();
+
+  static AnalysisMPI* GetAnalysis();
+
+  void Book();
+  void EndOfRun();
+
+  //void OpenFile(const G4String& fname);
+  void OpenFile();
+  void Write();
+  void CloseFile(G4bool reset = true);
+
+  void ClearIncidentFlag();
+
+private:
+  AnalysisMPI();
+  DISALLOW_COPY_AND_ASSIGN(AnalysisMPI);
+
+  static G4ThreadLocal G4int fincidentFlag;
+};
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+inline void AnalysisMPI::ClearIncidentFlag()
+{
+  fincidentFlag = false;
+}
+
+#endif
---
+++ src/AnalysisMPI.cc
@@ -0,0 +1,139 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+/// @file AnalysisMPI.cc
+/// @brief Define histograms and ntuples
+
+#include "G4AutoDelete.hh"
+#include "G4SystemOfUnits.hh"
+#include "AnalysisMPI.hh"
+#include "G4MPImanager.hh"
+
+//Select format of output here
+//Note: ntuple merging is supported only with Root format
+#include "g4root.hh"
+#include "G4RootAnalysisManager.hh"
+
+
+G4ThreadLocal G4int AnalysisMPI::fincidentFlag = false;
+G4ThreadLocal AnalysisMPI* the_analysis = 0;
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+AnalysisMPI*
+AnalysisMPI::GetAnalysis()
+{
+  if (!the_analysis)
+    {
+      the_analysis = new AnalysisMPI();
+      G4AutoDelete::Register(the_analysis);
+    }
+  return the_analysis;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+AnalysisMPI::AnalysisMPI()
+{
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+void
+AnalysisMPI::Book()
+{
+  G4cout << "Analysis::Book start" << G4endl;
+
+  G4AnalysisManager* analysisManager = G4AnalysisManager::Instance();
+
+  /*#ifdef G4MULTITHREADED
+  // MT ntuple merging
+  analysisManager->SetNtupleMerging(true);
+  #endif*/
+
+//----------------histograms---------------------------------------------------
+  analysisManager->CreateH1("sGamma","Gamma-ray source",2000,0,20); // 0
+  analysisManager->CreateH1("sNeutron","Neutron source",2000,0,20); // 1
+  analysisManager->CreateH1("sAlpha","alpha source",1000,0,10);     // 2
+  
+  analysisManager->CreateH1("sGammaOut","Gamma-rays exiting cask",2000,0,20); // 3
+  analysisManager->CreateH1("sNeutronOut","Neutrons exiting cask",2000,0,20); // 4
+//----------------root tree / ntuple---------------------------------------
+  analysisManager->CreateNtuple("SplitPoints", "SplitPoints");
+  analysisManager->CreateNtupleIColumn("eventID");
+  analysisManager->CreateNtupleIColumn("PDGcode");
+  analysisManager->CreateNtupleDColumn("Time");
+  analysisManager->CreateNtupleDColumn("Energy");
+  analysisManager->CreateNtupleDColumn("posX");
+  analysisManager->CreateNtupleDColumn("posY");
+  analysisManager->CreateNtupleDColumn("posZ");
+  analysisManager->CreateNtupleDColumn("dirX");
+  analysisManager->CreateNtupleDColumn("dirY");
+  analysisManager->CreateNtupleDColumn("dirZ");
+  analysisManager->CreateNtupleDColumn("weight");
+  analysisManager->FinishNtuple();
+
+  analysisManager->CreateNtuple("FUEL-PIN", "ResponseNtuple");
+  analysisManager->CreateNtupleIColumn("EventID");
+  analysisManager->CreateNtupleIColumn("DetectorNbr");
+  analysisManager->CreateNtupleIColumn("PDGcode");
+  analysisManager->CreateNtupleDColumn("TimeOfEvent");
+  analysisManager->CreateNtupleDColumn("TimeInEvent");
+  analysisManager->CreateNtupleDColumn("Light");
+  analysisManager->CreateNtupleDColumn("Weight");
+  analysisManager->FinishNtuple();
+
+  G4cout << "AnalysisMPI::Book finished " << G4endl;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+AnalysisMPI::~AnalysisMPI()
+{
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+void
+AnalysisMPI::OpenFile()
+{
+  G4AnalysisManager* mgr = G4AnalysisManager::Instance();
+  G4String filename = mgr->GetFileName();
+  G4int rank = G4MPImanager::GetManager()->GetRank();
+  filename += "_r" + std::to_string(rank);
+  mgr->OpenFile(filename.c_str());
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+void
+AnalysisMPI::Write()
+{
+  G4AnalysisManager* mgr = G4AnalysisManager::Instance();
+  mgr->Write();
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+void
+AnalysisMPI::CloseFile(G4bool reset)
+{
+  G4AnalysisManager* mgr = G4AnalysisManager::Instance();
+  mgr->CloseFile(reset);
+}
